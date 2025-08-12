89fc30e5d3b1895e4fa213a491c3dcb72ad45313
Anoushka Samanta <anoushka1701@gmail.com>
2025-08-12 18:33:36 +0530

    Issue 5 Fixed:Added Privacy Policy and Terms of Service Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import Home from './components/Home';
 import Navbar from './components/Navbar';
 import React, { Component } from 'react';
 import Footer from './components/Footer';
+import PrivacyPolicyPage from './pages/PrivacyPolicyPage';
+import TermsOfServicePage from './pages/TermsofServicePage';
 
 class ErrorBoundary extends Component {
   state = { hasError: false };
@@ -26,6 +28,8 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/privacy" element={<PrivacyPolicyPage/>} />
+          <Route path="/terms" element={<TermsOfServicePage/>} />
           <Route path="/create-room" element={<div className="text-white text-center p-10">Create Room Page</div>} />
           <Route path="/join-room" element={<div className="text-white text-center p-10">Join Room Page</div>} />
           <Route path="/about" element={<div className="text-white text-center p-10">About Page</div>} />
